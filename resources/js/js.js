6a17a0016b05ed25d427425b5b64fc4b3e74f171
Fernando aguiar <fernando2015.aguiar@gmail.com>
2024-02-03 01:09:55 -0300

    reestruturação de pastas, composer, frameworks e cdns importados pelo proprio projeto

--- resources/js/js.js
+++ resources/js/js.js
@@ -9,38 +9,38 @@ $(document).ready(function(){
     });
 
     // Inicialização do carrossel usando o plugin 'slick' para a seção de vídeos
-    // $('.videos .itens').slick({
-    //     dots: true,
-    //     arrows: false,                
-    //     infinite: true,            
-    //     speed: 300,
-    //     autoplay: true,               
-    //     slidesToShow: 3,           
-    //     slidesToScroll: 3,         
-    //     responsive: [
-    //         {
-    //             breakpoint: 1024,   
-    //             settings: {
-    //                 slidesToShow: 3,
-    //                 slidesToScroll: 3
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 600,    
-    //             settings: {
-    //                 slidesToShow: 2,
-    //                 slidesToScroll: 2
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 480,    
-    //             settings: {
-    //                 slidesToShow: 1,
-    //                 slidesToScroll: 1
-    //             }
-    //         }
-    //     ]
-    // });
+    $('.videos .itens').slick({
+        dots: true,
+        arrows: false,                
+        infinite: true,            
+        speed: 300,
+        autoplay: true,               
+        slidesToShow: 3,           
+        slidesToScroll: 3,         
+        responsive: [
+            {
+                breakpoint: 1024,   
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 600,    
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,    
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 
    $('a').on('click', function(){
         if($(this).is('[href]')){
--- src/Views/commons/footer.php
+++ src/Views/commons/footer.php
@@ -2,11 +2,12 @@
 		<div class="container">
 			<div class="conteudo">
 				<p>Todos os direitos reservados</p>
-				<p>Desenvolvido por <a href="#">DevMorais<i class="fab fa-instagram"></i></a></p>
+				<p>Desenvolvido por <a href="#">DevMorais <i class="fab fa-instagram"></i></a></p>
 			</div>
 		</div>
 	</footer>
     <script src="<?=URL_BASE?>resources/js/jquery/jquery.min.js"></script>
+    <script src="<?=URL_BASE?>resources/js/slick.min.js"></script>
 	<script src="<?=URL_BASE?>resources/js/js.js"></script>
 </body>
 </html>
